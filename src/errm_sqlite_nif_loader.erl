fd65b4c510de5c4393c418595ce25243815f72a0
h4rl <h4rldev@proton.me>
2026-07-18 12:49:05 +0200

    feat: attempt at making escript extraction possible

--- src/errm_sqlite_nif_loader.erl
+++ src/errm_sqlite_nif_loader.erl
@@ -8,7 +8,7 @@ path(Module, BaseName) ->
     {error, bad_name} ->
       Root = ensure_extracted(),
       filename:join([Root, "lib", atom_to_list(Module), "priv", BaseName]);
-    PrivDir when is_list(PrivDir) ->
+    PrivDir ->
       filename:join([PrivDir, BaseName])
   end.
 
@@ -16,9 +16,36 @@ ensure_extracted() ->
   case persistent_term:get(?CACHE_KEY, undefined) of
     undefined ->
       Script = escript:script_name(),
-      {ok, Root} = escript:extract(Script, []),
-      persistent_term:put(?CACHE_KEY, Root),
-      Root;
-    Root ->
-      Root
+      if Script == undefined ->
+        erlang:error(no_escript_to_extract);
+        true ->
+          {ok, Sections} = escript:extract(Script, [{section, [body]}]),
+          ZipBinary = case proplists:get_value(zip, Sections) of
+            Zip when is_binary(Zip) -> Zip
+          end,
+          TempDir = create_temp_dir(),
+          ok = zip:unzip(ZipBinary, [{cwd, TempDir}]),
+          persistent_term:put(?CACHE_KEY, TempDir),
+          TempDir
+      end;
+    Root -> Root
+  end.
+
+
+create_temp_dir() ->
+  Base = case os:getenv("TMPDIR") of
+    false -> case os:getenv("TEMP") of
+      false -> "/tmp";
+      T -> T
+    end;
+    T -> T
+  end,
+  Ts = calendar:system_time_to_rfc3339(erlang:system_time(second), [{offset, "Z"}]),
+  Ts1 = re:replace(Ts, "\\s+", "_", [global, {return, list}]),
+  Suffix = integer_to_list(erlang:unique_integer([positive])),
+  Dir = filename:join([Base, "escript_extract_" ++ Ts1 ++ "_" ++ Suffix]),
+  case file:make_dir(Dir) of
+    ok -> Dir;
+    {error, eexist} -> create_temp_dir();
+    {error, Reason} -> erlang:error({temp_dir_failed, Reason})
   end.
